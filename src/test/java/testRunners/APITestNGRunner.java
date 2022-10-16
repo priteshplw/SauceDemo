04431e8f6096f5e4a3f283ae63d1efe4bf09252c
Pritesh <priteshplw29@gmail.com>
2022-10-16 12:51:50 +0530

    This is the update for the testng plugin

--- src/test/java/testRunners/APITestNGRunner.java
+++ src/test/java/testRunners/APITestNGRunner.java
@@ -3,9 +3,9 @@ package testRunners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/features/APITesting/REQRES",
-        glue = {"stepDefinations/api/reqres"},
-        plugin = {"json:target/jsonReports/APITest.json"},
+@CucumberOptions(features = "src/test/resources/features/APITesting/REQRES", glue = {"stepDefinations/api/reqres"},
+        plugin = {"json:target/jsonReports/APITest.json",
+        "testng:target/xmlReports/APITest.xml"},
         tags = "APITEST"
 )
 public class APITestNGRunner extends AbstractTestNGCucumberTests {
--- src/test/java/testRunners/SauceTestNGRunner.java
+++ src/test/java/testRunners/SauceTestNGRunner.java
@@ -3,9 +3,9 @@ package testRunners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/features",
-        glue = {"stepDefinations","hooks"},
-        plugin = {"json:target/jsonReports/UITest.json"},
+@CucumberOptions(features = "src/test/resources/features", glue = {"stepDefinations","hooks"},
+        plugin = {"json:target/jsonReports/UITest.json",
+                "testng:target/xmlReports/UITest.xml"},
         tags = "@UITest"
 )
 public class SauceTestNGRunner extends AbstractTestNGCucumberTests {
